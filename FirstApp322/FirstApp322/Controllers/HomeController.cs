44f14fa0c3944db254ae8ccb11e0413a65347f2d
ughurba <ughurba@edu.code.az>
2022-06-16 17:03:53 +0400

    feat

---
+++ FirstApp322/FirstApp322/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp322.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //ViewResult result = new ViewResult();
+
+            //return Json(
+            //    new
+            //    {
+            //        name="lorem",
+            //        age=45,
+            //    });
+            //return Ok("okay");
+            //return File("","");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
